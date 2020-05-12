8d10b711e80e3a9e8cd7dd4e5d5e56c6d8a391be
steven <steven.johan1999@gmail.com>
2020-05-12 07:32:31 +0700

    Shopping cart and payment design completed

--- cart.php
+++ cart.php
@@ -2,154 +2,68 @@
 <html lang="zxx">
 <?php include ('header.php');?>
 
-  <!-- slider Area Start-->
-  <div class="slider-area ">
-    <!-- Mobile Menu -->
-    <div class="single-slider slider-height2 d-flex align-items-center" data-background="assets/img/hero/category.jpg">
-        <div class="container">
-            <div class="row">
-                <div class="col-xl-12">
-                    <div class="hero-cap text-center">
-                        <h2>Order</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-  </div>
-  <!-- slider Area End-->
-
   <!--================Cart Area =================-->
   <section class="cart_area section_padding">
     <div class="container">
       <div class="cart_inner">
         <div class="table-responsive">
           <table class="table">
+            
+            <!-- 4 Kolom di cart -->
             <thead>
               <tr>
                 <th scope="col">Product</th>
                 <th scope="col">Price</th>
                 <th scope="col">Quantity</th>
-                <th scope="col">Total</th>
+                <th scope="col">Subtotal</th>
               </tr>
             </thead>
+
+            <!-- Barang-barang di cart -->
             <tbody>
               <tr>
                 <td>
                   <div class="media">
                     <div class="d-flex">
+                      <!-- GAMBAR PRODUK DISINI -->
                       <img src="assets/img/logo/logo-sovents.png" alt="" />
                     </div>
                     <div class="media-body">
-                      <p>Decor***</p>
+                      <!-- NAMA PRODUK DISINI -->
+                      <p>NATURAL RED BIRTHDAY PACK</p> 
                     </div>
                   </div>
                 </td>
                 <td>
+                  <!-- HARGA SATUAN BARANG DISINI -->
                   <h5>Rp.00.0</h5>
                 </td>
                 <td>
                   <div class="product_count">
-                    <!-- <input type="text" value="1" min="0" max="10" title="Quantity:"
-                      class="input-text qty input-number" />
-                    <button
-                      class="increase input-number-increment items-count" type="button">
-                      <i class="ti-angle-up"></i>
-                    </button>
-                    <button
-                      class="reduced input-number-decrement items-count" type="button">
-                      <i class="ti-angle-down"></i>
-                    </button> -->
+                    <!-- BUTTON TAMBAH DAN KURANGI QTY PRODUK -->
                     <span class="input-number-decrement"> <i class="ti-minus"></i></span>
-                    <input class="input-number" type="text" value="1" min="0" max="10">
-                    <span class="input-number-increment"> <i class="ti-plus"></i></span>
+                    <input class="input-number" type="text" value="1" min="0" max="99">
+                    <div class="input-number-increment"> <i class="ti-plus"></i></div>
                   </div>
                 </td>
                 <td>
+                  <!-- HARGA SATUAN * QTY PRODUK -->
                   <h5>Rp.o.oo</h5>
                 </td>
               </tr>
+              
+              <!-- Total semua biaya -->
               <tr>
+                <td></td>
+                <td></td>
                 <td>
-                  <div class="media">
-                    <div class="d-flex">
-                      <img src="assets/img/logo/logo-sovents.png" alt="" />
-                    </div>
-                    <div class="media-body">
-                      <p>Paket **</p>
-                    </div>
-                  </div>
-                </td>
-                <td>
-                  <h5>Rp.o.oo</h5>
-                </td>
-                <td>
-                  <div class="product_count">
-                      <span class="input-number-decrement"> <i class="ti-minus"></i></span>
-                      <input class="input-number" type="text" value="1" min="0" max="10">
-                      <span class="input-number-increment"> <i class="ti-plus"></i></span>
-                  </div>
+                  <h5>Total</h5>
                 </td>
                 <td>
                   <h5>Rp.o.oo</h5>
                 </td>
               </tr>
 
-              <tr>
-                <td></td>
-                <td></td>
-                <td>
-                  <h5>Subtotal</h5>
-                </td>
-                <td>
-                  <h5>Rp.o.oo</h5>
-                </td>
-              </tr>
-              <tr class="shipping_area">
-                <td></td>
-                <td></td>
-                <td>
-                  <h5>Shipping</h5>
-                </td>
-                <td>
-                  <div class="shipping_box">
-                    <ul class="list">
-                      <li>
-                        Flat Rate: Rp.o.oo
-                        <input type="radio" aria-label="Radio button for following text input">
-                      </li>
-                      <li>
-                        Free Shipping
-                        <input type="radio" aria-label="Radio button for following text input">
-                      </li>
-                      <li>
-                        Flat Rate: Rp.o.oo
-                        <input type="radio" aria-label="Radio button for following text input">
-                      </li>
-                      <li class="active">
-                        Local Delivery: Rp.o.oo
-                        <input type="radio" aria-label="Radio button for following text input">
-                      </li>
-                    </ul>
-                    <h6>
-                      Calculate Shipping
-                      <i class="fa fa-caret-down" aria-hidden="true"></i>
-                    </h6>
-                    <select class="shipping_select">
-                      <option value="1">SUMATERA</option>
-                      <option value="2">JABODETABEK</option>
-                      <option value="4">JAWA BARAT</option>
-                    </select>
-                    <select class="shipping_select section_bg">
-                      <option value="1">Select a State</option>
-                      <option value="2">Select a State</option>
-                      <option value="4">Select a State</option>
-                    </select>
-                    <input class="post_code" type="text" placeholder="Postcode/Zipcode" />
-                    <a class="btn_1" href="#">Update Details</a>
-                  </div>
-                </td>
-              </tr>
             </tbody>
           </table>
           <div class="checkout_btn_inner float-right">
